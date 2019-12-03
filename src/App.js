287d41b457d6582fd3ecd88e3932cad7716e123c
Piotr Filochowski <piotr.filochowski@novaris.pl>
2019-12-03 20:14:51 +0100

    some stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import MyDiagram from './components/MyDiagram';
-
+import Buttons from './components/Buttons'
 
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <Buttons/>
+                <br></br>
                 <MyDiagram />
             </header>
         </div>
